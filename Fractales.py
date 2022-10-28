8daad734f903e8ff8e94c5d39573f8aeac59323e
Gatien ACER <le.magicien.annonyme@gmail.com>
2022-10-28 15:10:25 +0200

    first commit

---
+++ Fractales.py
@@ -0,0 +1,114 @@
+import math
+
+##############################################################
+# fonctions sur les nombre complexes
+def i_plus(z1,z2):
+    return [z1[0]+z2[0],z1[1]+z2[1]]
+
+def i_moins(z1,z2):
+    return [z1[0]-z2[0],z1[1]-z2[1]]
+
+def i_fois(z1,z2):
+    a = z1[0]
+    b = z1[1]
+    c = z2[0]
+    d = z2[1]
+    return [a*c-b*d, a*d+b*c]
+
+def i_carre(z):
+    return i_fois(z,z)
+
+##############################################################
+# Fonction de dessin
+def print_zone(z, axe):
+    if axe:
+        z = set_axis(z)
+    for l in z:
+        for c in l:
+            print(c,end="")
+        print()
+
+
+def set_axis(z):
+    x, y = get_dimensions(z)
+    for i in range(2*y+1):
+        for j in range(2*x+1):
+            if i == y:
+                if j == x:
+                    if z[i][j] == ' ':
+                        z[i][j] = '┼'
+                    else:
+                        z[i][j] = '╬'
+                else:
+                    if z[i][j] == ' ':
+                        z[i][j] = '─'
+                    else:
+                        z[i][j] = '═'
+            else:
+                if j == x:
+                    if z[i][j] == ' ':
+                        z[i][j] = '│'
+                    else:
+                        z[i][j] = '║'
+                    
+    return z
+
+def get_zone(maxx, maxy):
+    zone = []
+    lignes = 2*maxy + 1
+    colonnes = 2*maxx + 1
+    zero = [maxy+1,maxx+1]
+    # zone de base
+    for i in range(lignes):
+        zone.append([" " for j in range(colonnes)])
+    return zone
+
+
+def get_dimensions(z):
+    y = len(z) // 2
+    x = len(z[0]) // 2
+    return [x,y]
+
+
+##############################################################
+# Fonction pour les fractales
+def z_carre_plus_c_boucle(z, c): # z² + c, avec c = a+ib
+    cpt = 0
+    while abs(z[0]+z[1]) < 10000 and cpt < 100:
+        cpt += 1
+        z = i_plus(i_carre(z), c)
+    return cpt >= 100
+
+
+
+def julia(zone, c): # z² + c
+    x, y = get_dimensions(zone)
+    for i in range(2*y+1):
+        for j in range(2*x+1):
+            if z_carre_plus_c_boucle([(j-x)/20,(i-y)/20],c):
+                zone[i][j] = '#'
+    return zone
+
+
+def mandelbrot(zone):
+    x, y = get_dimensions(zone)
+    for i in range(2*y+1):
+        for j in range(2*x+1):
+            if z_carre_plus_c_boucle([0, 0], [(j-x)/30, (i-y)/20]):
+                zone[i][j] = '#'
+    return zone
+
+
+
+
+# Fonction principale
+def main():
+    max_x = 50
+    max_y = 20
+    plan = get_zone(max_x, max_y)
+    axes = input("axes ? [0=Non, 1=Oui] : ") == "1"
+
+    print_zone(mandelbrot(plan), axes)
+
+
+main()
